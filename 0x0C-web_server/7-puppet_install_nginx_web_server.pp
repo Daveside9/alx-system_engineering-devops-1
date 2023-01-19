d3fe3d8fad16a1b5b65efb9e2da641a2990e3c6e
Oluwajuwon165 <99687642+Oluwajuwon165@users.noreply.github.com>
2023-01-19 19:08:48 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,20 @@
-# install and configure an Nginx server using Puppet instead of Bash
-
+# Install Nginx web server (w/ Puppet)
 package { 'nginx':
   ensure => installed,
-  name   => 'nginx'
 }
-file { 'path_to_html':
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
   content => 'Hello World!',
-  path    => '/var/www/html/index.html'
 }
-file_line { 'put_in_line':
-  ensure => present,
-  path   => 'etc/nginx/sites-available/default',
-  after  => '/listen 80 default_server;'
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
-}
+
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx']
+  require => Package['nginx'],
 }
